2a917c039675881d3c32421d89b076fa9ff8df7d
Mark Temple-Heald <mtempleheald@hotmail.com>
2021-02-20 17:27:39 +0000

    access canvas after page load

--- www/index.js
+++ www/index.js
@@ -1,3 +1,12 @@
 import * as wasm from "rust-wasm";
 
 wasm.greet();
+
+const canvas = document.getElementById("appCanvas");
+const gl = canvas.getContext("webgl", { antialias: true});
+
+if (!gl) {
+    alert('Failed to initialize WebGL');
+}
+
+const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
